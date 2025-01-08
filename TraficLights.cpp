c960371b18348edccc341e6a444742aeb3a7a75d
UmairKaleem <umairkaleem017@gmail.com>
2025-01-08 23:45:08 +0500

    changed

--- TraficLights.cpp
+++ TraficLights.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
-#include <string>
 using namespace std;
-int ain()
+int main()
 {
-    string color;
-    cout << "Enter the color of the traffic light: ";
-    cin >> color;
-    if (color == "red")
+ 
+
+    cout << "Welcome to Trafic Light Management System" << endl<<endl;
+    while(1)
     {
+    cout << "Select the color of the traffic light: " << endl;
+    cout << "1-red------>Stop" << endl;
+    cout << "2-yellow--->Ready" << endl;
+    cout << "3-green---->Go" << endl;
+    cout << "4-Exit" << endl;
+
+    int colors;
+    cout << "Enter the number of the color: ";
+    cin >> colors;
+    
+    if (colors==4)
+    {
+        break;
+    }
+
+    switch(colors)
+    {
+        case 1:
         cout << "Stop" << endl;
-    }
-    else if (color == "yellow")
-    {
-        cout << "Slow down" << endl;
-    }
-    else if (color == "green")
-    {
+        break;
+
+        case 2:
+        cout << "Ready" << endl;
+        break;
+
+        case 3:
         cout << "Go" << endl;
-    }
-    else
-    {
+        break;
+
+        default:
         cout << "Invalid color" << endl;
+        break;
     }
-    return 0;
+
+    }
+    cout<<"you have exited the system"<<endl;
+    cout<<"Thank you for using Trafic Light Management System"<<endl;
+
 }
